d56e675b0bb69e04acdd0a131c0e18117ca450d3
SuXine <suyuxuan0401@163.com>
2018-11-20 00:33:09 +0800

    no message

--- cn-libra-user/src/main/java/cn/libra/user/service/UserService.java
+++ cn-libra-user/src/main/java/cn/libra/user/service/UserService.java
@@ -1,13 +1,20 @@
 package cn.libra.user.service;
 
 
-
-import cn.libra.utils.util.redis.RedisUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service("user_userService")
 @Transactional(readOnly = true)
 public class UserService {
+
+
+
+    public JSONObject hello(JSONObject params){
+        JSONObject json = new JSONObject();
+        json.put("hello","hello");
+        return json;
+
+    }
 }
