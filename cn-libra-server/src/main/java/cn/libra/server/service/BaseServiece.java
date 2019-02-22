651a577cb1cc93a993ab4af4a2f67a0086188e19
xuhan <xuhan@spacetech.com.cn>
2019-02-22 17:43:54 +0800

    修改登入接口

--- cn-libra-server/src/main/java/cn/libra/server/service/BaseServiece.java
+++ cn-libra-server/src/main/java/cn/libra/server/service/BaseServiece.java
@@ -1,19 +1,27 @@
 package cn.libra.server.service;
 
 import cn.libra.server.bean.User;
-import cn.libra.server.bean.UserExample;
 import cn.libra.server.dao.UserMapper;
+import cn.libra.utils.util.Base64Util;
+import cn.libra.utils.util.StringUtil;
 import cn.libra.utils.util.exception.ControllerException;
+import cn.libra.utils.util.redis.RedisUtil;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Base64Utils;
 
-import java.util.List;
+import java.util.Date;
+
 
 @Component @Transactional(readOnly = true) public class BaseServiece {
 
 	@Autowired UserMapper userMapper;
+
+
+	@Autowired RedisUtil redisUtil;
+
 
 	/**
 	 * API: server_base_login / 中文名
@@ -21,9 +29,14 @@ import java.util.List;
 	 * @program:params
 	 * @author: sx
 	 **/
-	public String login(JSONObject params) {
+	public JSONObject login(JSONObject params)  {
 		String loginName = params.getString("loginName");
 		String password = params.getString("password");
+
+		redisUtil.set(loginName,password);
+		redisUtil.set(loginName,"5555555555");
+		Object obj = redisUtil.get(loginName);
+		System.out.println(StringUtil.getNullStr(obj));
 
 //		UserExample userExample = new UserExample();
 //		userExample.createCriteria().andAccEqualTo(acc);
@@ -45,7 +58,18 @@ import java.util.List;
 			throw new ControllerException("密码错误");
 		}
 
-		return "SUCCESS";
+		JSONObject jsonObject = new JSONObject();
+		Date date = new Date();
+
+		String token = Base64Util.encode("loginName:"+loginName+";"+"loginTime:"+StringUtil.getNullStr(date)+";"+"xuhan;");
+
+
+		redisUtil.set("token",token,(long)1);
+
+		jsonObject.put("token",token);
+
+
+		return jsonObject;
 	}
 
 
